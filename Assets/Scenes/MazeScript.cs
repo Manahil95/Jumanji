4f210d9445047f331ba6c9f88fc097651fef7c6b
MuznaMSK <53683966+MuznaMSK@users.noreply.github.com>
2019-09-17 16:09:36 +0400

    maze

---
+++ Assets/Scenes/MazeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray,out hit))
+        {
+            if(hit.transform.name == "MoveRight")
+            {
+
+            }
+            if (hit.transform.name == "MoveLeft")
+            {
+
+            }
+            if (hit.transform.name == "Walk")
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
