a05b899571111c4bf7c9323dc1ccb26d6e03a477
Alrubkhi96 <53684001+Alrubkhi96@users.noreply.github.com>
2019-09-19 10:34:52 +0400

    Start Oculus

---
+++ Assets/BottleAndPuzzle/Scripts/hold.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hold : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+
+    ////private Vector3 screenPoint;
+    ////private Vector3 offset;
+
+    ////void OnMouseDown()
+    ////{
+    ////    screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+    ////    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    ////}
+
+    ////void OnMouseDrag()
+    ////{
+    ////    Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+    ////    Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) - offset;
+
+    ////    transform.position = curPosition;
+
+    ////}
+
+    //void OnMouseDrag()
+    //{
+    //    float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    //    transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+
+    //}
+
+
+}
