b38c3c0d01e7f1182a7e90719e7dfa5c35a0d569
MuznaMSK <53683966+MuznaMSK@users.noreply.github.com>
2019-09-19 14:09:27 +0400

    scripting

---
+++ Assets/Scenes/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "CloseButton")
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scenes/MazeMoveScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeMoveScript : MonoBehaviour
+{
+    
+    private float Speed = 0.3f;
+    public GameObject Enemy;
+    
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (gameObject.tag == "Walk")
+            {
+                Enemy.transform.position += Enemy.transform.TransformDirection(Vector3.forward);
+            }
+            if (gameObject.tag == "WalkBack")
+            {
+                Enemy.transform.position += Enemy.transform.TransformDirection(Vector3.back);
+            }
+            if (gameObject.tag == "MoveRight")
+            {
+                Enemy.transform.position += Enemy.transform.TransformDirection(Vector3.right);
+            }
+            if (gameObject.tag == "MoveLeft")
+            {
+                Enemy.transform.position += Enemy.transform.TransformDirection(Vector3.left);
+            }
+        }
+    }
+}
+
