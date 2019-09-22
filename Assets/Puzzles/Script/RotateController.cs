734e6e4fdf6ce926b0d151151a8e14eb6f856d83
Manahil95 <53684015+Manahil95@users.noreply.github.com>
2019-09-22 14:04:03 +0400

    initial version

---
+++ Assets/Puzzles/Script/RotateController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour
+{
+    public List<GameObject> WaterConnector;
+   
+    public float time;
+    bool direction;
+    bool move;
+
+    void RotateObjects()
+    {
+        time += Time.deltaTime;
+
+
+        if (direction)
+        {
+
+            transform.eulerAngles = Vector3.Lerp(Vector3.zero, Vector3.up * 90, time);
+            foreach (var item in WaterConnector)
+            {
+                item.transform.eulerAngles = Vector3.Lerp(Vector3.zero, Vector3.up * 90, time);
+            }
+        }
+        else
+        {
+
+            transform.eulerAngles = Vector3.Lerp(Vector3.up * 90, Vector3.zero, time);
+            foreach (var item in WaterConnector)
+            {
+                item.transform.eulerAngles = Vector3.Lerp(Vector3.up * 90, Vector3.zero, time);
+            }
+            
+        }
+
+        
+    }
+
+    private void OnMouseDown()
+    {
+        time = 0;
+        direction = !direction;
+        InvokeRepeating("RotateObjects", 0, Time.deltaTime);
+
+        //if (!move)
+        //{
+        //    CancelInvoke("RotateObjects");
+        //}
+    }
+
+   
+
+
+}
