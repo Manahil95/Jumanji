33b611f38e13508a9b887a01b92712fb972cc751
Manahil95 <53684015+Manahil95@users.noreply.github.com>
2019-09-29 15:05:59 +0400

    first update

--- Assets/Puzzles/Script/RotateController.cs
+++ Assets/Puzzles/Script/RotateController.cs
@@ -1,39 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotateController : MonoBehaviour
 {
     public List<WaterTank> WaterConnector;
+    Quaternion targetRotation;
+    //Rigidbody rb;
 
     float time;
     bool move;
     float YRotation;
 
-    Quaternion targetRotation;
+    Vector3 handInitialPos;
+    Vector3 handCurrentPos;
 
-    private void Start()
-    {
-        //YRotation = transform.rotation.y;
-        //angel = YRotation + 90;
-    }
+    public Transform hands;
 
     void RotateObjects()
     {
         time += Time.deltaTime;
-
+        handCurrentPos = hands.position;
+        YRotation = Vector3.Angle(handInitialPos, handCurrentPos);
+        myText.text = YRotation.ToString();
+        targetRotation = Quaternion.Euler(0, YRotation, 0.0f);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
 
-        foreach (var item in WaterConnector)
-        {
-            item.transform.rotation = Quaternion.RotateTowards(item.transform.rotation, item.TargetRotation, 100 * Time.deltaTime);
-        }
+        //foreach (var item in WaterConnector)
+        //{
+        //    item.transform.rotation = Quaternion.RotateTowards(item.transform.rotation, item.TargetRotation, 100 * Time.deltaTime);
+        //}
 
-        if (time >= 1)
-        {
-            CancelInvoke();
-            move = false;
-        }
+        //if (time >= 1)
+        //{
+        //    CancelInvoke();
+        //    move = false;
+        //}
     }
 
     private void OnMouseDown()
@@ -53,6 +57,28 @@ public class RotateController : MonoBehaviour
 
             InvokeRepeating("RotateObjects", 0, Time.deltaTime);
         }
+    }
+
+    public Text myText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!move)
+        {
+            if (collision.gameObject.tag == "hand")
+            {
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+                {
+                    handInitialPos = hands.position;
+                    InvokeRepeating("RotateObjects", 0, Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        CancelInvoke();
     }
 
 }
