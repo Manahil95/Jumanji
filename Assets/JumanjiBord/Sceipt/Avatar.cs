58b105928ebe73c5fcad7233caccdd9d78301df1
Manahil95 <53684015+Manahil95@users.noreply.github.com>
2019-10-10 10:52:18 +0400

    jumanji bord

---
+++ Assets/JumanjiBord/Sceipt/Avatar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : MonoBehaviour
+{
+    public Transform[] wayPoints;
+    int wayPointsIndex;
+
+    Vector3 intPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
