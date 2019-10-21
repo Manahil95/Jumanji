af0eaef4ffa9923ff47cad1090cd1b398aaa71ac
Alrubkhi96 <f.alrubkhi@gmail.com>
2019-10-21 10:08:25 +0400

    Before add Text

--- Assets/BottleAndPuzzle/Scripts/winpointscipt.cs
+++ Assets/BottleAndPuzzle/Scripts/winpointscipt.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class winpointscipt : MonoBehaviour
 {
-    public GameObject box;
-    public Material Material1;
+
     int i = 0;
     public void Increment()
     {
         i++;
         if (i == 3)
         {
-            box.GetComponent<MeshRenderer>().material = Material1;
+
         }
     }
 
