a8e5af7e48dfa57a4982fbfc7e2bc69304b7657d
Alrubkhi96 <f.alrubkhi@gmail.com>
2019-10-22 18:36:46 +0400

    Not Done yet

---
+++ Assets/Main Menu/Scripts/UIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject JumanjiTitlePanel;
+    public Animation JumanjiTitleAnimation;
+    private float open = 5.0f;
+    private float delete = 10.0f;
+
+    public GameObject JumanjiwordPanel;
+    private float OpenumanjiwordPanel = 11.0f;
+    private float DeleteumanjiwordPanel = 20.0f;
+
+    void Start()
+    {
+       Invoke("ShowJumanjiTitlePanel", open);
+       Invoke("HideJumanjiTitlePanel", delete);
+
+       Invoke("ShowJumanjiwordPanel", OpenumanjiwordPanel);
+       Invoke("HideJumanjiwordPanel", DeleteumanjiwordPanel);
+    }
+    void Update()
+    {
+
+    }
+    void ShowJumanjiTitlePanel()
+    {
+        JumanjiTitlePanel.gameObject.SetActive(true);
+        
+    }
+    void HideJumanjiTitlePanel()
+    {
+        JumanjiTitlePanel.gameObject.SetActive(false);
+    }
+    void ShowJumanjiwordPanel()
+    {
+        JumanjiwordPanel.gameObject.SetActive(true);
+    }
+    void HideJumanjiwordPanel()
+    {
+        JumanjiwordPanel.gameObject.SetActive(false);
+    }
+}
