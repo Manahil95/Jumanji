2f876d0b7755e0af1b0d5ac32537fc6ada537e0d
Manahil95 <53684015+Manahil95@users.noreply.github.com>
2019-10-28 14:34:33 +0400

    Dice 12

--- Assets/JumanjiBord/Sceipt/DiceCheckZoneScript.cs
+++ Assets/JumanjiBord/Sceipt/DiceCheckZoneScript.cs
@@ -22,21 +22,39 @@ public class DiceCheckZoneScript : MonoBehaviour
                 switch (col.gameObject.name)
                 {
                     case "Side1":
-                        DiceNumberTextScript.diceNumber1 = 6;
+                        DiceNumberTextScript.diceNumber1 = 12;
                         break;
                     case "Side2":
-                        DiceNumberTextScript.diceNumber1 = 5;
+                        DiceNumberTextScript.diceNumber1 = 11;
                         break;
                     case "Side3":
-                        DiceNumberTextScript.diceNumber1 = 4;
+                        DiceNumberTextScript.diceNumber1 = 10;
                         break;
                     case "Side4":
-                        DiceNumberTextScript.diceNumber1 = 3;
+                        DiceNumberTextScript.diceNumber1 = 9;
                         break;
                     case "Side5":
-                        DiceNumberTextScript.diceNumber1 = 2;
+                        DiceNumberTextScript.diceNumber1 = 8;
                         break;
                     case "Side6":
+                        DiceNumberTextScript.diceNumber1 = 7;
+                        break;
+                    case "Side7":
+                        DiceNumberTextScript.diceNumber1 = 6;
+                        break;
+                    case "Side8":
+                        DiceNumberTextScript.diceNumber1 = 5;
+                        break;
+                    case "Side9":
+                        DiceNumberTextScript.diceNumber1 = 4;
+                        break;
+                    case "Side10":
+                        DiceNumberTextScript.diceNumber1 = 3;
+                        break;
+                    case "Side11":
+                        DiceNumberTextScript.diceNumber1 = 2;
+                        break;
+                    case "Side12":
                         DiceNumberTextScript.diceNumber1 = 1;
                         break;
                 }
